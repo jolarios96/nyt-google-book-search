e9efdb4db8912048991fd6507cf036a8513a66cc
Jordan Larios <jolarios96@gmail.com>
2019-01-28 16:59:24 -0800

    added nav, jumbotron. prepped for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import Form from "./components/Form";
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Jumbotron} />
+            {/* <Route exact path="/books" component={Books} />
+            <Route exact path="/books/:id" component={Detail} />
+            <Route component={NoMatch} /> */}
+          </Switch>
+          <Route exact path="/" component={Form} />
+        </div >
+      </Router>
     );
   }
 }
