253fd2185bc5d9e1187f7a3514efef9bf553315a
Jordan Larios <jolarios96@gmail.com>
2019-01-28 16:41:00 -0800

    added navbar component

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+function Nav() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+            <a class="navbar-brand" href="/">Google Books</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Search</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="saved">Saved</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
+
+
