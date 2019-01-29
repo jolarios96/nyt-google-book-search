f2ab00940c143b86ca409f8b82faec5c1f65b9df
Jordan Larios <jolarios96@gmail.com>
2019-01-28 16:57:42 -0800

    added Jumbotron component

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron">
+            <h1>(React) Google Books Search</h1>
+            <p>Search for and Save Books of Interest</p>
+        </div>
+    );
+}
+
+export default Jumbotron;
